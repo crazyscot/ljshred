86029e7b17852cd7152d1b2a7c70866bf045c6c5
Ross Younger <onyx-commits@impropriety.org.uk>
2017-04-09 18:09:06 +1200

    Add --mixed-mode

--- ljshred.py
+++ ljshred.py
@@ -165,6 +165,12 @@ def entry_to_garbage(lj,event):
     lj.server.LJ.XMLRPC.editevent(lj.auth_headers(args))
     # response data ignored
 
+MIXED_MODE_MODELIST=[entry_to_garbage, entry_to_blocks]
+
+def mixed_mode(lj,event):
+    ''' Randomly calls on to another mode '''
+    return random.choice(MIXED_MODE_MODELIST)(lj,event)
+
 def delete_entry(lj,event):
     '''
     Callback which deletes entry
@@ -274,6 +280,7 @@ def parse_args(args=sys.argv[1:]):
     group.add_argument('--printout', dest='action_callback', action='store_const', const=print_entry, help='Only prints out all the entries it would touch, doesn\'t actually change anything.')
     group.add_argument('--block-out', dest='action_callback', action='store_const', const=entry_to_blocks, help='Replaces all non-whitespace text in all entries with a solid block character')
     group.add_argument('--random-garbage', dest='action_callback', action='store_const', const=entry_to_garbage, help='Replaces entries with random garbage text')
+    group.add_argument('--mixed-mode', dest='action_callback', action='store_const', const=mixed_mode, help='A mixture of --random-garbage and --block-out modes')
     group.add_argument('--delete', dest='action_callback', action='store_const', const=delete_entry, help='Deletes entries')
 
     return vars(parser.parse_args(args))

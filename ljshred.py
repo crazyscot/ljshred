2dda69a3f5d1c4071c53fb857332deab7e244d53
Ross Younger <onyx-commits@impropriety.org.uk>
2017-04-14 19:43:53 +1200

    Add --zalgo mode, with caveat

--- ljshred.py
+++ ljshred.py
@@ -161,6 +161,74 @@ def standard_args_for(event, subject, text):
         args['subject'] = subject
     return args
 
+# List of zalgo characters taken from https://gist.github.com/MetroWind/1401473
+ZALGO_UP = [u'\u030D', u'\u030E', u'\u0304', u'\u0305', u'\u033F',
+        u'\u0311', u'\u0306', u'\u0310', u'\u0352', u'\u0357',
+        u'\u0351', u'\u0307', u'\u0308', u'\u030A', u'\u0342',
+        u'\u0343', u'\u0344', u'\u034A', u'\u034B', u'\u034C',
+        u'\u0303', u'\u0302', u'\u030C', u'\u0350', u'\u0300',
+        u'\u0301', u'\u030B', u'\u030F', u'\u0312', u'\u0313',
+        u'\u0314', u'\u033D', u'\u0309', u'\u0363', u'\u0364',
+        u'\u0365', u'\u0366', u'\u0367', u'\u0368', u'\u0369',
+        u'\u036A', u'\u036B', u'\u036C', u'\u036D', u'\u036E',
+        u'\u036F', u'\u033E', u'\u035B', u'\u0346', u'\u031A']
+
+ZALGO_MID = [u'\u0315', u'\u031B', u'\u0340', u'\u0341', u'\u0358',
+        u'\u0321', u'\u0322', u'\u0327', u'\u0328', u'\u0334',
+        u'\u0335', u'\u0336', u'\u034F', u'\u035C', u'\u035D',
+        u'\u035E', u'\u035F', u'\u0360', u'\u0362', u'\u0338',
+        u'\u0337', u'\u0361', u'\u0489']
+
+ZALGO_DOWN = [u'\u0316', u'\u0317', u'\u0318', u'\u0319', u'\u031C',
+        u'\u031D', u'\u031E', u'\u031F', u'\u0320', u'\u0324',
+        u'\u0325', u'\u0326', u'\u0329', u'\u032A', u'\u032B',
+        u'\u032C', u'\u032D', u'\u032E', u'\u032F', u'\u0330',
+        u'\u0331', u'\u0332', u'\u0333', u'\u0339', u'\u033A',
+        u'\u033B', u'\u033C', u'\u0345', u'\u0347', u'\u0348',
+        u'\u0349', u'\u034D', u'\u034E', u'\u0353', u'\u0354',
+        u'\u0355', u'\u0356', u'\u0359', u'\u035A', u'\u0323']
+
+def zalgochar(c):
+    ''' Zalgoises a single character. Returns a unicode string. '''
+    # Don't zalgoise a zalgo character, that way lies madness
+    if c in ZALGO_UP or c in ZALGO_MID or c in ZALGO_DOWN:
+        return c
+    rv = [c]
+    rv += [ random.choice(ZALGO_UP) for _ in range(random.randint(1,8)) ]
+    rv += [ random.choice(ZALGO_MID) for _ in range(random.randint(0,2)) ]
+    rv += [ random.choice(ZALGO_DOWN) for _ in range(random.randint(1,8)) ]
+    return ''.join(rv)
+
+def zalgoise_string(s, maxlen):
+    '''
+    Function to zalgoise a Unicode string, with a really naive way of making sure we fall inside the given length limit
+    '''
+    s = ''.join([zalgochar(c) for c in unicode(s)])
+    while len(s.encode('utf-8')) > maxlen:
+        s=s[0:len(s)/2]
+    return s
+
+def zalgoise_entry(lj, event):
+    '''
+    Callback which zalgoises the entry text. This one is reversible by
+    filtering out the zalgo characters, at least on short entries.
+    For a heavier zalgoisation, run the tool a second time.
+
+    Zalgoisation is all about Unicode combining characters.
+    See http://stackoverflow.com/questions/6579844/how-does-zalgo-text-work
+
+    Unfortunately LJ has fixed size limits on entry text and subjects.
+    Adding UTF-8 combining characters very quickly hits the limit for a
+    subject line, so it's not worth doing those.
+    '''
+    text = zalgoise_string(xmlrpc_to_unicode(event['event']), 65535)
+    try:
+        subject = xmlrpc_to_unicode(event['subject'])
+    except KeyError: # subject not in entry
+        subject = None
+    lj.server.LJ.XMLRPC.editevent(lj.auth_headers(standard_args_for(event, subject, text)))
+    # response data ignored
+
 def chickenise(lj,event):
     '''
     Callback which replaces words with "chicken", leaving punctuation (non-word characters) untouched.
@@ -333,6 +401,7 @@ def parse_args(args=sys.argv[1:]):
     group.add_argument('--chicken', dest='action_callback', action='store_const', const=chickenise, help='Replaces all words with the word "chicken"')
     group.add_argument('--mixed-mode', dest='action_callback', action='store_const', const=mixed_mode, help='Applies one of --random-garbage, --block-out or --chicken to each entry in turn')
     group.add_argument('--delete', dest='action_callback', action='store_const', const=delete_entry, help='Deletes entries')
+    group.add_argument('--zalgo', dest='action_callback', action='store_const', const=zalgoise_entry, help='Zalgoises the text (adds a bunch of Unicode combining characters which smear a mess over them). This conversion is largely reversible, and it doesn\'t look very good in all browsers.')
 
     return vars(parser.parse_args(args))
 
